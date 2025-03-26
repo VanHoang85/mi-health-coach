8b9fa7006a5e485e3ff7a96633f747092ee5a1b6
VanHoang85 <khanhvan8591@gmail.com>
2025-03-26 00:16:02 +0000

    remove timeout

--- model_utils/agent.py
+++ model_utils/agent.py
@@ -38,10 +38,10 @@ class Agent:
                                           messages=messages,
                                           temperature=self.args.temperature,
                                           top_p=self.args.top_p,
-                                          num_retries=self.args.num_retries,
-                                          stream_timeout=self.args.stream_timeout if "stream_timeout" in self.args else None,
-                                          request_timeout=self.args.request_timeout if "request_timeout" in self.args else None,  # raise Timeout error if call takes longer than 10s
-                                          fallbacks=self.args.fallback_models if "fallback_models" in self.args else None,
+                                          # num_retries=self.args.num_retries,
+                                          # stream_timeout=self.args.stream_timeout if "stream_timeout" in self.args else None,
+                                          # request_timeout=self.args.request_timeout if "request_timeout" in self.args else None,  # raise Timeout error if call takes longer than 10s
+                                          # fallbacks=self.args.fallback_models if "fallback_models" in self.args else None,
                                           stream=stream
                                           )  # may add fallback
             if not stream:
